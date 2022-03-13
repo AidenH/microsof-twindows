81c4330484708e746f378a47242baab33a8c5f7f
AidenH <aidenalexh@gmail.com>
2022-03-13 18:56:58 -0400

    limited add-window tiling added

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,113 @@ struct State<'a> {
     scr: &'a x::Screen,
     wins: Vec<Window>,
     curr_win: Vec<Window>,
+    item_list: Vec<WindowItem>,
 }
 
-fn add_window(con: &xcb::Connection, w: Window) -> xcb::Result<()> {
-    let cookie = con.send_request_checked(&x::ConfigureWindow {
+#[derive(Debug)]
+struct WindowItem {
+    id: usize,
+    splits_from: Vec<usize>,
+    splits_into: Vec<usize>,
+    window: Window,
+    x: i32,
+    y: i32,
+    width: u32,
+    height: u32,
+}
+
+fn add_window(mut state: State, w: Window) -> xcb::Result<State> {
+    let mut win_item: WindowItem;
+    let border: u32 = 2;
+
+    // default full screen if no other windows open
+    win_item = WindowItem {
+        id: state.item_list.len(),
+        splits_from: Vec::<usize>::new(),
+        splits_into: Vec::<usize>::new(),
+        window: w,
+        x: 0,
+        y: 0,
+        width: state.scr.width_in_pixels() as u32,
+        height: state.scr.height_in_pixels() as u32,
+    };
+
+    // if other windows open, modify sizes based on window new window will be split from
+    if !state.item_list.is_empty() {
+        let parent = state.item_list
+            .iter()
+            .position(|x| x.window == state.curr_win[0] )
+            .unwrap();
+
+        win_item.splits_from.push(state.item_list[parent].id);
+        state.item_list[parent].splits_into.push(win_item.id);
+
+        if win_item.width > win_item.height { // vertical split
+            win_item.x = state.item_list[parent].width as i32 / 2;
+            win_item.y = state.item_list[parent].y;
+            win_item.width = state.item_list[parent].width / 2;
+            win_item.height = state.item_list[parent].height;
+
+            state.item_list[parent].width = win_item.width;
+        } else {
+            win_item.x = state.item_list[parent].x;
+            win_item.y = state.item_list[parent].height as i32 / 2;
+            win_item.width = state.item_list[parent].width;
+            win_item.height = state.item_list[parent].height / 2;
+
+            state.item_list[parent].height = win_item.height;
+        }
+    }
+
+    state.item_list.push(win_item);
+
+    println!("{:?}", state.item_list);
+    for i in &state.item_list {
+        let cookie = state.con.send_request_checked(&x::ConfigureWindow {
+            window: i.window,
+            value_list: &[
+                x::ConfigWindow::X(i.x),
+                x::ConfigWindow::Y(i.y),
+                x::ConfigWindow::Width(i.width-border-2),
+                x::ConfigWindow::Height(i.height-border-2),
+                x::ConfigWindow::BorderWidth(border),
+            ],
+        });
+
+        state.con.check_request(cookie)?;
+
+        let cookie = state.con.send_request_checked(&x::ChangeWindowAttributes {
+            window: i.window,
+            value_list: &[
+                x::Cw::BorderPixel(0x444444),
+                x::Cw::EventMask(EventMask::KEY_PRESS |
+                                 EventMask::ENTER_WINDOW |
+                                 EventMask::LEAVE_WINDOW |
+                                 EventMask::STRUCTURE_NOTIFY),
+            ]
+        });
+
+        state.con.check_request(cookie)?;
+
+        state.con.send_request(&x::MapWindow {
+            window: i.window,
+        });
+    }
+
+    /*let cookie = state.con.send_request_checked(&x::ConfigureWindow {
         window: w,
         value_list: &[
-            x::ConfigWindow::X(100),
-            x::ConfigWindow::Y(100),
-            x::ConfigWindow::Width(400),
-            x::ConfigWindow::Height(400),
-            x::ConfigWindow::BorderWidth(2),
+            x::ConfigWindow::X(win_item.x),
+            x::ConfigWindow::Y(win_item.y),
+            x::ConfigWindow::Width(win_item.width-border-2),
+            x::ConfigWindow::Height(win_item.height-border-2),
+            x::ConfigWindow::BorderWidth(border),
         ],
     });
 
-    con.check_request(cookie)?;
+    state.con.check_request(cookie)?;
 
-    let cookie = con.send_request_checked(&x::ChangeWindowAttributes {
+    let cookie = state.con.send_request_checked(&x::ChangeWindowAttributes {
         window: w,
         value_list: &[
             x::Cw::BorderPixel(0x444444),
@@ -31,15 +121,15 @@ fn add_window(con: &xcb::Connection, w: Window) -> xcb::Result<()> {
         ]
     });
 
-    con.check_request(cookie)?;
+    state.con.check_request(cookie)?;
 
-    con.send_request(&x::MapWindow {
+    state.con.send_request(&x::MapWindow {
         window: w,
-    });
+    });*/
 
-    con.flush()?;
+    state.con.flush()?;
 
-    Ok(())
+    Ok(state)
 }
 
 fn destroy_win(state: &State) -> xcb::Result<usize> {
@@ -83,10 +173,6 @@ fn focus(opt: bool, con: &xcb::Connection,  win: Window) -> xcb::Result<()> {
     Ok(())
 }
 
-fn redraw_tiles() {
-    // tiling logic
-}
-
 fn main() -> xcb::Result<()> {
     let (connection, scr_num) = xcb::Connection::connect(None).unwrap();
     let setup = connection.get_setup();
@@ -97,6 +183,7 @@ fn main() -> xcb::Result<()> {
         scr: &screen,
         wins: Vec::<Window>::new(),
         curr_win: Vec::<Window>::new(),
+        item_list: Vec::<WindowItem>::new(),
     };
 
     let cookie = state.con.send_request_checked(&x::ChangeWindowAttributes {
@@ -164,7 +251,8 @@ fn main() -> xcb::Result<()> {
             }
 
             xcb::Event::X(x::Event::MapRequest(_e)) => {
-                add_window(&state.con, _e.window())?;
+                println!("add");
+                state = add_window(state, _e.window())?;
                 state.wins.push(_e.window());
             }
 
