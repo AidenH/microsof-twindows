e20d02a3257504a360e56c842ca38ea71b5db4ac
AidenH <aidenalexh@gmail.com>
2022-03-12 19:49:54 -0500

    main: 'state' covering common objects

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::process::Command;
 
 use xcb::{x::{self, Window}, x::EventMask};
 
+struct State<'a> {
+    con: &'a xcb::Connection,
+    scr: &'a x::Screen,
+    wins: Vec<Window>,
+    curr_win: Vec<Window>,
+}
+
 fn add_window(con: &xcb::Connection, w: Window) -> xcb::Result<()> {
     let cookie = con.send_request_checked(&x::ConfigureWindow {
         window: w,
@@ -35,14 +42,14 @@ fn add_window(con: &xcb::Connection, w: Window) -> xcb::Result<()> {
     Ok(())
 }
 
-fn destroy_win(con: &xcb::Connection, win_list: &Vec<Window>, e: Window) -> xcb::Result<usize> {
-    let cookie = con.send_request_checked(&x::DestroyWindow {
-        window: e,
+fn destroy_win(state: &State) -> xcb::Result<usize> {
+    let cookie = state.con.send_request_checked(&x::DestroyWindow {
+        window: state.curr_win[0],
     });
 
-    con.check_request(cookie)?;
+    state.con.check_request(cookie)?;
 
-    let a = win_list.iter().position(|&x| x == e).unwrap();
+    let a = state.wins.iter().position(|&x| x == state.curr_win[0]).unwrap();
 
     Ok(a)
 }
@@ -81,25 +88,29 @@ fn redraw_tiles() {
 }
 
 fn main() -> xcb::Result<()> {
-    let mut win_list = Vec::<Window>::new(); // list of all open windows
-    let mut curr_win = Vec::<Window>::new(); // currently focused window
+    let (connection, scr_num) = xcb::Connection::connect(None).unwrap();
+    let setup = connection.get_setup();
+    let screen = setup.roots().nth(scr_num as usize).unwrap();
 
-    let (con, scr_num) = xcb::Connection::connect(None).unwrap();
-    let setup = con.get_setup();
-    let scr = setup.roots().nth(scr_num as usize).unwrap();
+    let mut state = State {
+        con: &connection,
+        scr: &screen,
+        wins: Vec::<Window>::new(),
+        curr_win: Vec::<Window>::new(),
+    };
 
-    let cookie = con.send_request_checked(&x::ChangeWindowAttributes {
-        window: scr.root(),
+    let cookie = state.con.send_request_checked(&x::ChangeWindowAttributes {
+        window: state.scr.root(),
         value_list: &[
-            x::Cw::BackPixel(scr.black_pixel()),
+            x::Cw::BackPixel(state.scr.black_pixel()),
             x::Cw::EventMask(EventMask::KEY_PRESS | EventMask::SUBSTRUCTURE_REDIRECT),
         ]
     });
 
-    con.check_request(cookie)?;
+    state.con.check_request(cookie)?;
 
     loop {
-        match con.wait_for_event()? {
+        match state.con.wait_for_event()? {
             xcb::Event::X(x::Event::KeyPress(e)) => {
 
                 if e.detail() == 26 &&
@@ -121,40 +132,40 @@ fn main() -> xcb::Result<()> {
                 } else if e.detail() == 24 &&
                     e.state() == x::KeyButMask::MOD1 | x::KeyButMask::SHIFT { // alt 'q'
 
-                    if !curr_win.is_empty() && !win_list.is_empty() {
-                        let remove_win = destroy_win(&con, &win_list, curr_win[0]).unwrap();
-                        win_list.remove(remove_win);
+                    if !state.curr_win.is_empty() && !state.wins.is_empty() {
+                        let remove_win = destroy_win(&state).unwrap();
+                        state.wins.remove(remove_win);
                     }
                 }
             }
 
             xcb::Event::X(x::Event::UnmapNotify(_e)) => {
-                let remove_win = win_list.iter().position(|&x| x == _e.event());
+                let remove_win = state.wins.iter().position(|&x| x == _e.event());
                 match remove_win {
                     Some(win) => {
-                        win_list.remove(win);
+                        state.wins.remove(win);
                     }
                     None => {}
                 }
             }
 
             xcb::Event::X(x::Event::EnterNotify(_e)) => {
-                curr_win.push(_e.event());
-                focus(true, &con, _e.event())?;
+                state.curr_win.push(_e.event());
+                focus(true, &state.con, _e.event())?;
             }
 
             xcb::Event::X(x::Event::LeaveNotify(_e)) => {
-                curr_win.pop();
+                state.curr_win.pop();
 
                 // if win_list contains _e's Window
-                if win_list.iter().any(|&x| x == _e.event()) {
-                    focus(false, &con, _e.event())?;
+                if state.wins.iter().any(|&x| x == _e.event()) {
+                    focus(false, &state.con, _e.event())?;
                 }
             }
 
             xcb::Event::X(x::Event::MapRequest(_e)) => {
-                add_window(&con, _e.window())?;
-                win_list.push(_e.window());
+                add_window(&state.con, _e.window())?;
+                state.wins.push(_e.window());
             }
 
             _ => {}
